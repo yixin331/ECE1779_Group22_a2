07d066cb0bbb416f69cdc4ba20593a0906edd3b7
Ubuntu <ubuntu@ip-172-31-85-21.ec2.internal>
2022-10-16 21:31:22 -0400

    change start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,10 @@
 #! /bin/bash
 #pip install -r requirements.txt
-mysql --user=root --password=ece1779pass  -e "source frontend/app/initializeDB.sql"
+mysql --user=root --password=ece1779pass  -e "source A_1/frontend/app/initializeDB.sql"
+tmux new -d -s memcache "python3 A_1/memcache/run.py"
+
+python3 A_1/frontend/run.py
 pkill -f tmux
-
-tmux new -d -s memcache "python3 memcache/run.py"
-
-#tmux new -d -s frontend "python3 frontend/run.py"
-python3 frontend/run.py
 echo "Server ended!"
 
 
