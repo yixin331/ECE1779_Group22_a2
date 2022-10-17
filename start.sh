60e1dab275455681fba5e76d4ce83b72c0828c1c
yixin331 <yixin331@users.noreply.github.com>
2022-10-16 21:34:03 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,11 @@
 #! /bin/bash
 #pip install -r requirements.txt
 mysql --user=root --password=ece1779pass  -e "source A_1/frontend/app/initializeDB.sql"
-pkill -f tmux
 
 tmux new -d -s memcache "python3 A_1/memcache/run.py"
 
-#tmux new -d -s frontend "python3 A_1/frontend/run.py"
 python3 A_1/frontend/run.py
+pkill -f tmux
 echo "Server ended!"
 
 
