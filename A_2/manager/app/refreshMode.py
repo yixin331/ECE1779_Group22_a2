159cdf79058b84bf2689e3b224dc30c94f32d3a8
yixin331 <yixin331@users.noreply.github.com>
2022-11-13 14:38:56 -0500

    float cast for threshold and ratio

--- A_2/manager/app/refreshMode.py
+++ A_2/manager/app/refreshMode.py
@@ -58,7 +58,7 @@ def refreshMode():
         mode = request.form['mode']
         memcache_mode['mode'] = mode
         # manual_mode
-        if (mode == 'Manual'):
+        if mode == 'Manual':
             # pause scheduler
             try:
                 requests.post(url='http://localhost:5003/setMode', data=request.form)
@@ -66,23 +66,23 @@ def refreshMode():
                 webapp.logger.warning("Autoscaler loses connection")
 
             num_node = int(request.form['num_node'])
-            if (num_node != memcache_mode['num_node']):
+            if not num_node == memcache_mode['num_node']:
                 try:
                     numToSend = {'num_node': num_node}
                     requests.post(url='http://localhost:5000/sizeChange', data=numToSend)
                 except requests.exceptions.ConnectionError as err:
                     webapp.logger.warning("Frontend loses connection")
 
-        elif (mode == 'Auto'):
-            memcache_mode['max_thr'] = request.form['max_thr']
-            memcache_mode['min_thr'] = request.form['min_thr']
-            memcache_mode['expand_ratio'] = request.form['expand_ratio']
-            memcache_mode['shrink_ratio'] = request.form['shrink_ratio']
+        elif mode == 'Auto':
+            memcache_mode['max_thr'] = float(request.form.get('max_thr'))
+            memcache_mode['min_thr'] = float(request.form.get('min_thr'))
+            memcache_mode['expand_ratio'] = float(request.form.get('expand_ratio'))
+            memcache_mode['shrink_ratio'] = float(request.form.get('shrink_ratio'))
             try:
                 dataToSend = {'num_node': memcache_mode['num_node'], 'mode': request.form['mode'], 'max_thr': request.form['max_thr'], 'min_thr': request.form['min_thr'], 'expand_ratio': request.form['expand_ratio'], 'shrink_ratio': request.form['shrink_ratio']}
                 requests.post(url='http://localhost:5003/setMode', data=dataToSend)
             except requests.exceptions.ConnectionError as err:
                 webapp.logger.warning("Autoscaler loses connection")
 
-        dbconnection.put_mode(int(memcache_mode['num_node']), memcache_mode['mode'], memcache_mode['max_thr'], memcache_mode['min_thr'], memcache_mode['expand_ratio'], memcache_mode['shrink_ratio'])
+        dbconnection.put_mode(memcache_mode['num_node'], memcache_mode['mode'], memcache_mode['max_thr'], memcache_mode['min_thr'], memcache_mode['expand_ratio'], memcache_mode['shrink_ratio'])
         return render_template("mode.html", num_node=memcache_mode['num_node'], result="Mode has been reset")
