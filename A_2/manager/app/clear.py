16d3df7f409df9b3820be8d82fa75da1718fd24f
yixin331 <yixin331@users.noreply.github.com>
2022-11-14 15:51:06 -0500

    remap ordered by time and add condition of ip is None

--- A_2/manager/app/clear.py
+++ A_2/manager/app/clear.py
@@ -7,11 +7,12 @@ import requests
 def clear():
     if request.method == 'POST':
         for id, ip in node_ip.items():
-            node_address = 'http://' + ip + ':5001/clearCache'
-            try:
-                response = requests.post(url=node_address).json()
-            except requests.exceptions.ConnectionError as err:
-                webapp.logger.warning("Cache loses connection")
+            if not ip == None:
+                node_address = 'http://' + ip + ':5001/clearCache'
+                try:
+                    response = requests.post(url=node_address).json()
+                except requests.exceptions.ConnectionError as err:
+                    webapp.logger.warning("Cache loses connection")
         result = "Your cache has been cleared"
         return render_template("clear.html",result=result)
     else:
