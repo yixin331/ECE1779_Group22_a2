f5b6db82707e61e81d2b186064be9ea6254988ab
yixin331 <yixin331@users.noreply.github.com>
2022-11-20 15:03:25 -0500

    Update __init__.py

--- A_2/frontend/app/__init__.py
+++ A_2/frontend/app/__init__.py
@@ -10,8 +10,6 @@ from app import main
 from app import get
 from app import put
 from app import key
-from app import refreshConfiguration
-from app import stat
 from app import deleteAll
 from app import sizeChange
 
