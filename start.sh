7b4617d978b423314d0210eacd1a01d14f50e10b
yinfeis <99629488+yinfeis@users.noreply.github.com>
2022-11-20 17:00:31 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,9 +3,9 @@ pip install -r requirements.txt
 mysql mysql --host=ece1779-a2.cedgw5dhsseo.us-east-1.rds.amazonaws.com --port=3306 --user=root --password=ece1779pass -e "source A_2/frontend/app/initializeDB.sql"
 
 tmux new -d -s autoscaler "python3 A_2/autoscaler/run.py"
-tmux new -d -s manager "python3 A_2/manager/run.py"
+tmux new -d -s frontend "python3 A_2/frontend/run.py"
 
 
-python3 A_2/frontend/run.py
+python3 A_2/manager/run.py
 pkill -f tmux
 echo "Server ended!"
